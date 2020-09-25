392441e91bff6a2b53facc1b6b9dc4a03ab1d57f
joshand@cisco.com <joshand@cisco.com>
2020-09-25 13:46:00 -0500

    search for scripts in global tenant as well

--- scripts/common.py
+++ scripts/common.py
@@ -2,7 +2,9 @@ from appicm.models import *
 
 
 def get_script(pm):
-    uz = Upload.objects.filter(tenant=pm.tenant).filter(description=pm.py_mod_name + ".py")
+    uz = Upload.objects.filter(tenant_id=pm.tenant.id).filter(description=pm.py_mod_name + ".py")
+    if len(uz) == 0:
+        uz = Upload.objects.filter(tenant_id=get_default_tenant()).filter(description=pm.py_mod_name + ".py")
     if len(uz) > 0:
         pmn = "scripts." + str(uz[0].filename()).replace(".py", "")
         return pmn
