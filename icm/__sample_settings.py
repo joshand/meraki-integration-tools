b12f3b587a5410fc13e02e93683a080e22fc0e83
joshand@cisco.com <joshand@cisco.com>
2020-11-10 11:09:19 -0600

    make settings sample settings

--- icm/__sample_settings.py
+++ icm/__sample_settings.py
@@ -80,18 +80,18 @@ WSGI_APPLICATION = 'icm.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.mysql',
-    #     'NAME': 'icm',
-    #     'USER': 'icm',
-    #     'PASSWORD': 'password',
-    #     'HOST': 'localhost',
-    #     'PORT': '',
-    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'icm',
+        'USER': 'icm',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '',
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
 }
 
 
