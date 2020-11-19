a36b1a2a1d87819a16c43850a86db337bcb3ad54
joshand@cisco.com <joshand@cisco.com>
2020-11-19 12:30:59 -0600

    updates to client

--- scripts/tunnel_client.py
+++ scripts/tunnel_client.py
@@ -98,10 +98,12 @@ def default_route():
 
 def run():
     reg_url = controller + "/api/v0/tunnels/" + myuuid + "/register"
-    print(reg_url)
     data = {"app": app_name, "ver": app_version}
     r = requests.post(reg_url, json=data)
     # print(r.content.decode("utf-8"))
     rj = r.json()
-    start_tunnel(rj["portnum"])
-    app.run(host="0.0.0.0", port=8000, debug=False)
+    if "portnum" in rj:
+        start_tunnel(rj["portnum"])
+        app.run(host="0.0.0.0", port=8000, debug=False)
+    else:
+        print("Error; no port returned", str(rj))
