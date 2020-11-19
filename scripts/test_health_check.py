db9ef0304ede9f54e46fd37376dd825fc618ae74
joshand@cisco.com <joshand@cisco.com>
2020-11-19 16:13:48 -0600

    add health check test

--- scripts/test_health_check.py
+++ scripts/test_health_check.py
@@ -5,8 +5,8 @@ from scripts.tunnels import start_tunnel, stop_tunnel, health_check
 def tunnel_health_check():
     tcs = TunnelClient.objects.all()
     for tc in tcs:
-        print("health_check on ", tc)
-        state, result = health_check(tc.get_internal_port())
+        print("health_check on", tc)
+        state, result = health_check(tc.get_internal_port(), timeout=10)
         print(state, result)
         if not state:
             stop_tunnel(tc.pid)
