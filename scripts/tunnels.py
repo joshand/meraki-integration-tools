55ece3b75e3e0be08d44d727189fe375570f2241
joshand@cisco.com <joshand@cisco.com>
2020-11-19 10:47:42 -0600

    refactor to retry on busy port

--- scripts/tunnels.py
+++ scripts/tunnels.py
@@ -3,16 +3,29 @@ import os
 import signal
 
 
-def start_tunnel(inportnum, portnum, tunnel_client):
-    cmd = "pproxy -l tunnel://:" + inportnum + " -r tunnel+in://:" + portnum + " -v"
-    # print(cmd)
-    sp = subprocess.Popen(cmd.split(" "), stderr=subprocess.PIPE, stdout=subprocess.PIPE)
-    # s = subprocess.check_output([cmd], stderr=subprocess.STDOUT)
-    # print(p)
-    out, err = sp.communicate()
-    tunnel_client.pid = sp.pid
-    tunnel_client.log = cmd + "\n" + str(out) + "\n" + str(err) + "\n" + str(sp.returncode)
-    tunnel_client.save()
+def start_tunnel(tunnel_client):
+    sp = None
+    while True:
+        inportnum = str(tunnel_client.get_internal_port())
+        outportnum = str(tunnel_client.tunnelport.portnumber)
+
+        cmd = "pproxy -l tunnel://:" + inportnum + " -r tunnel+in://:" + outportnum + " -v"
+        # print(cmd)
+        sp = subprocess.Popen(cmd.split(" "), stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+        # s = subprocess.check_output([cmd], stderr=subprocess.STDOUT)
+        # print(p)
+        out, err = sp.communicate()
+        out_txt = str(out.decode("utf-8"))
+        err_txt = str(err.decode("utf-8"))
+        if "address already in use" in out_txt or "address already in use" in err_txt:
+            tunnel_client.tunnelport = tunnel_client.find_open_port()
+            tunnel_client.save()
+        else:
+            tunnel_client.pid = sp.pid
+            tunnel_client.log = cmd + "\n" + out_txt + "\n" + err_txt + "\n" + str(sp.returncode)
+            tunnel_client.save()
+            break
+
     return sp
 
 
