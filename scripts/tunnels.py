1a46071fec15cdc69b4ead6b990160efce940abe
joshand@cisco.com <joshand@cisco.com>
2020-11-19 10:17:14 -0600

    improve logging and add stop_tunnel

--- scripts/tunnels.py
+++ scripts/tunnels.py
@@ -1,13 +1,20 @@
-from subprocess import Popen
+import subprocess
+import os
+import signal
 
 
 def start_tunnel(inportnum, portnum, tunnel_client):
     cmd = "pproxy -l tunnel://:" + inportnum + " -r tunnel+in://:" + portnum + " -v"
     # print(cmd)
-    p = Popen(cmd.split(" "))
+    sp = subprocess.Popen(cmd.split(" "), stderr=subprocess.PIPE, stdout=subprocess.PIPE)
     # s = subprocess.check_output([cmd], stderr=subprocess.STDOUT)
     # print(p)
-    tunnel_client.pid = p.pid
-    tunnel_client.log = cmd + "\n" + str(p)
+    out, err = sp.communicate()
+    tunnel_client.pid = sp.pid
+    tunnel_client.log = cmd + "\n" + str(out) + "\n" + str(err) + "\n" + str(sp.returncode)
     tunnel_client.save()
-    return p
+    return sp
+
+
+def stop_tunnel(pid):
+    os.kill(pid, signal.SIGTERM)  # or signal.SIGKILL
