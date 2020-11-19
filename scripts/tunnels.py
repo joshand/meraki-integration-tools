3ef16c7d572d5faa24bc07a3bafd794512e2061e
joshand@cisco.com <joshand@cisco.com>
2020-11-19 10:57:33 -0600

    change db order wiht subprocess

--- scripts/tunnels.py
+++ scripts/tunnels.py
@@ -14,14 +14,18 @@ def start_tunnel(tunnel_client):
         sp = subprocess.Popen(cmd.split(" "), stderr=subprocess.PIPE, stdout=subprocess.PIPE)
         # s = subprocess.check_output([cmd], stderr=subprocess.STDOUT)
         # print(p)
+        tunnel_client.pid = sp.pid
+        tunnel_client.log = ""
+        tunnel_client.save()
+
         out, err = sp.communicate()
         out_txt = str(out.decode("utf-8"))
         err_txt = str(err.decode("utf-8"))
         if "address already in use" in out_txt or "address already in use" in err_txt:
+            tunnel_client.log = cmd + "\nPort in use... relaunching..."
             tunnel_client.tunnelport = tunnel_client.find_open_port()
             tunnel_client.save()
         else:
-            tunnel_client.pid = sp.pid
             tunnel_client.log = cmd + "\n" + out_txt + "\n" + err_txt + "\n" + str(sp.returncode)
             tunnel_client.save()
             break
