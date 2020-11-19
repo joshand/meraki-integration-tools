1646849ff62fc33ca7688e0b2ee85010f8e7a505
joshand@cisco.com <joshand@cisco.com>
2020-11-19 16:11:22 -0600

    add health check test

---
+++ scripts/test_health_check.py
@@ -0,0 +1,18 @@
+from appicm.models import *
+from scripts.tunnels import start_tunnel, stop_tunnel, health_check
+
+
+def tunnel_health_check():
+    tcs = TunnelClient.objects.all()
+    for tc in tcs:
+        print("health_check on ", tc)
+        state, result = health_check(tc.get_internal_port())
+        print(state, result)
+        if not state:
+            stop_tunnel(tc.pid)
+            time.sleep(5)
+            start_tunnel(tc)
+
+
+def run():
+    tunnel_health_check()
